9fdba38964994c2f47aaa16ea7e6e6f497a6daff
r6eve <r6eve179f@gmail.com>
2018-08-28 14:42:07 +0900

    fix an order of application #2

--- src/util.ml
+++ src/util.ml
@@ -2,16 +2,16 @@ let flip f x y = f y x
 
 let all_color_regexp = Re.Perl.compile_pat "\x1b\\[(\\d*;?\\d*m|K)"
 
+let ampersand = Re.Perl.compile_pat "&"
 let apostrophe = Re.Perl.compile_pat "'"
 let quotedbl = Re.Perl.compile_pat "\""
-let ampersand = Re.Perl.compile_pat "&"
 let less = Re.Perl.compile_pat "<"
 let greater = Re.Perl.compile_pat ">"
 
 let escape_html str =
   str
+  |> Re.replace_string ampersand ~by:"&#38;"
   |> Re.replace_string apostrophe ~by:"&#39;"
   |> Re.replace_string quotedbl ~by:"&#34;"
-  |> Re.replace_string ampersand ~by:"&#38;"
   |> Re.replace_string less ~by:"&#60;"
   |> Re.replace_string greater ~by:"&#62;"
