4562fef2a5ab04afa73736a760d0a720995bc3f9
r6eve <r6eve179f@gmail.com>
2018-09-12 14:35:37 +0900

    [ci skip] Add some comments

--- src/util.mli
+++ src/util.mli
@@ -1,3 +1,5 @@
 val flip : ('a -> 'b -> 'c) -> 'b -> 'a -> 'c
+(** flappers *)
 
 val all_color_regexp : Re.re
+(** Regular expression for 256 colors. *)
--- src/w3m.mli
+++ src/w3m.mli
@@ -1,5 +1,9 @@
 val make_process : unit -> out_channel
+(** Make w3m process for redirecting the standard input of the command to a
+    pipe. *)
 
 val output : out_channel -> string list -> unit
+(** Write the list of strings on the given output channel. *)
 
 val close_process : out_channel -> unit
+(** Close w3m process. *)
