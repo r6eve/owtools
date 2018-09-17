7cdce615f8cc4dd4b9163bcf051d8624490d681d
r6eve <r6eve179f@gmail.com>
2018-09-17 17:58:28 +0900

    Add some tests

--- test/extensions.ml
+++ test/extensions.ml
@@ -1,1 +1,9 @@
-let () = () (* TODO: *)
+open Owtools
+module String = Extensions.String
+
+let () =
+  assert (String.quote_glob "foo*bar" = "'foo*bar'");
+  assert (String.quote_glob "foobar" = "foobar");
+  assert (
+    String.escape_html "\"><script>alert(document.cookie)</script>"
+    = "&#34;&#62;&#60;script&#62;alert(document.cookie)&#60;/script&#62;")
