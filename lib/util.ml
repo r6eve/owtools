1423ac20a61c0fd1f4dccb870021b8baeb369e3c
r6eve <r6eve179f@gmail.com>
2019-03-10 13:21:57 +0900

    Change LICENSE to Boost Software License

--- lib/util.ml
+++ lib/util.ml
@@ -1,3 +1,12 @@
+(****************************************************************)
+(*                                                              *)
+(*           Copyright r6eve 2019 -                             *)
+(*  Distributed under the Boost Software License, Version 1.0.  *)
+(*     (See accompanying file LICENSE_1_0.txt or copy at        *)
+(*           https://www.boost.org/LICENSE_1_0.txt)             *)
+(*                                                              *)
+(****************************************************************)
+
 let flip f x y = f y x
 
 let some_of_x x = Some x
