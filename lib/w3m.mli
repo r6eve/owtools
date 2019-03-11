26322ec81519f5ade975de9a36ef7a5571de89b3
r6eve <r6eve179f@gmail.com>
2019-03-11 14:16:32 +0900

    Some tweaks

--- lib/w3m.mli
+++ lib/w3m.mli
@@ -1,11 +1,9 @@
-(****************************************************************)
-(*                                                              *)
-(*           Copyright r6eve 2019 -                             *)
-(*  Distributed under the Boost Software License, Version 1.0.  *)
-(*     (See accompanying file LICENSE_1_0.txt or copy at        *)
-(*           https://www.boost.org/LICENSE_1_0.txt)             *)
-(*                                                              *)
-(****************************************************************)
+(*
+ *           Copyright r6eve 2019 -
+ *  Distributed under the Boost Software License, Version 1.0.
+ *     (See accompanying file LICENSE_1_0.txt or copy at
+ *           https://www.boost.org/LICENSE_1_0.txt)
+ *)
 
 val make_process : unit -> out_channel
 (** Make w3m process for redirecting the standard input of the command to a
