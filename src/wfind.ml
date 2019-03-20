7f1f484824da80163d8b27e74a9b1bc3b2035453
r6eve <r6eve179f@gmail.com>
2019-03-20 16:19:03 +0900

    Fix LICENSE

--- src/wfind.ml
+++ src/wfind.ml
@@ -1,10 +1,8 @@
-(****************************************************************)
-(*                                                              *)
-(*           Copyright r6eve 2019 -                             *)
-(*  Distributed under the Boost Software License, Version 1.0.  *)
-(*     (See accompanying file LICENSE_1_0.txt or copy at        *)
-(*           https://www.boost.org/LICENSE_1_0.txt)             *)
-(*                                                              *)
-(****************************************************************)
+(*
+ *           Copyright r6eve 2019 -
+ *  Distributed under the Boost Software License, Version 1.0.
+ *     (See accompanying file LICENSE_1_0.txt or copy at
+ *           https://www.boost.org/LICENSE_1_0.txt)
+ *)
 
 let () = Owtools.Wfind.main ()
